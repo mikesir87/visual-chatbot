cf5c37e86c6ed05e16f1c591b4e8e91d086c379c
Michael Irwin <mikesir87@gmail.com>
2025-07-18 16:52:31 -0400

    Align settings dropdown to end of items

--- client/src/components/settings/SettingsDropdown.jsx
+++ client/src/components/settings/SettingsDropdown.jsx
@@ -18,7 +18,7 @@ export const SettingsDropdown = () => {
 
   return (
     <>
-      <NavDropdown title="Settings" id="settings-dropdown">
+      <NavDropdown title="Settings" id="settings-dropdown" align="end">
         <NavDropdown.Item onClick={() => setShowLlmConfigModal(true)} href="#">LLM backend</NavDropdown.Item>
         <NavDropdown.Item onClick={() => setSystemPromptModal(true)} href="#">System prompt</NavDropdown.Item>
         <NavDropdown.Item onClick={() => setShowAiToolGenerationModal(true)} href="#">AI tool generation</NavDropdown.Item>
